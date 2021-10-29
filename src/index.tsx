c7f4087febc6d84cc0832d7c6585790a6d579eb0
shikelong <578622705@qq.com>
2021-10-29 17:45:28 +0900

    build: add css support

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,15 @@
 import classNames from 'classnames';
 import React, { ElementType, useMemo } from 'react';
 import './floating-label-wrapper.css';
-import { defaultValueGetter } from './utils';
+import { defaultValueGetter, defaultVariables } from './utils';
 
 type Styling = Partial<{
   className: string;
   style: React.CSSProperties;
+  cssVariables: Partial<typeof defaultVariables>;
 }>;
 
-const defaultVariables = {
-  '--input-padding-y': '1em',
-  '--input-padding-x': '0.6em',
-  '--floating-label-padding-left': '0.3em',
-  '--floating-label-padding-right': '0.5em',
-  '--floating-label-font-size': '0.9em',
-  '--floating-label-font-size--floating': '0.75em',
-  '--floating-label-color': 'rgba(0, 0, 0, 0.5)',
-  '--active-color': 'rgb(43, 116, 226)',
-};
-
 export type FloatingLabelWrapperProps = Styling & {
-  cssVariables?: Partial<typeof defaultVariables>;
   label: string;
   children: JSX.Element;
   //the root container's element type.
--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,14 @@ export const isChildrenValid = (children: React.ReactChild): boolean => {
 export const defaultValueGetter = (props: any) => {
   return props.value;
 };
+
+export const defaultVariables = {
+  '--input-padding-y': '1em',
+  '--input-padding-x': '0.6em',
+  '--floating-label-padding-left': '0.3em',
+  '--floating-label-padding-right': '0.5em',
+  '--floating-label-font-size': '0.9em',
+  '--floating-label-font-size--floating': '0.75em',
+  '--floating-label-color': 'rgba(0, 0, 0, 0.5)',
+  '--active-color': 'rgb(43, 116, 226)',
+};
